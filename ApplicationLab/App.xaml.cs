cfac45667b4fe166c7c069d333240ed753da4a2e
untold-titan <untoldtitan3k@gmail.com>
2024-03-04 11:31:30 -0700

    Add project files.

---
+++ ApplicationLab/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ApplicationLab
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
